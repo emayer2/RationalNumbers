41be74fc826a66a0ea1c709d3439ccc16606b2e7
emayer2 <bohemian.mayer@gmail.com>
2018-05-10 16:44:58 -0700

    Fixed sh

--- eval-331-code.sh
+++ eval-331-code.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 git clone https://github.com/emayer2/RationalNumbers.git
+find src/hw4 -name "*.java" > @sources.txt
+javac -cp ".:junit.jar:/tmp/RationalNumbers/test-bayes.jar;" @sources.txt
 cd RationalNumbers/src/hw4/test
-for i in `seq 1 100`;
+javac -cp ".:junit.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest.java
+for i in `seq 100`;
 do
-    javac -cp ".:junit.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest.java
-    java -cp ".:junit.jar:hamcrest-core.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest
+    if [ $i -eq 1 || $i -eq 100 ]; then
+        java -cp ".:junit.jar:hamcrest-core.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest
+    else
+        java -cp ".:junit.jar:hamcrest-core.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest > /dev/null
+    fi
 done
