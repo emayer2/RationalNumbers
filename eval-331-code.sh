25182eb4de0df9940bd9ba71c85514484acf548c
emayer2 <bohemian.mayer@gmail.com>
2018-05-10 16:21:48 -0700

    Error fix

--- eval-331-code.sh
+++ eval-331-code.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git clone https://github.com/emayer2/RationalNumbers.git
 cd RationalNumbers/src/hw4/test
-javac -cp ".:junit.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest.java
-java -cp ".:junit.jar:hamcrest-core.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest
+for i in `seq 1 100`;
+do
+    javac -cp ".:junit.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest.java
+    java -cp ".:junit.jar:hamcrest-core.jar:/tmp/RationalNumbers/test-bayes.jar;" RatPolyTest
+done
